55390ce9ac33cfdf6c870be5a2ea386f1d7c7b47
Leo Knudsen <lk22>
2024-07-20 23:03:52 +0200

    trying to push a new commit to railway project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-	return res.send('<h1>Hello From Railway</h1>');
+	return res.send('<h1>Hello From Railway</h1><h2>Im a new headline</h2>');
 })
 
 app.listen(3000, function(){
